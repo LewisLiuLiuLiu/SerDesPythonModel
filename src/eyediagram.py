a734c16a21bb6678a36970ef359fd6b8314815c0
LewisLiuLiuLiu <45811021+LewisLiuLiuLiu@users.noreply.github.com>
2023-09-04 12:28:21 +0800

    Update eyediagram.py
    add comments about the future features:
    1. plot eyediagram
    2. eye width/height measurement
    3, plot Bathtub curve

--- src/eyediagram.py
+++ src/eyediagram.py
@@ -1,2 +1,10 @@
 import numpy as np
 import matplotlib.pyplot as plt
+
+"""
+  This file has several functions：
+  1. plot eyediagram
+  2. Measure Eye Height
+  3. Measure Eye width
+  4. Plot Bathtub curve
+"""
